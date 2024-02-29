f29b1d94709276ba376fdfbbd6007e714156bc4a
DiegoRAP2003 <diegoalfaropinto12@gmail.com>
2024-02-29 09:10:11 -0600

    home page lab10 y actualizacion de nombre de archivos

--- lab_10/server.js
+++ lab_10/server.js
@@ -2,28 +2,25 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-const server = http.createServer((request, response) => {
+function handleRequest(request, response, htmlFileName = "index.html") {
   let filePath;
 
-  // Manejar la ruta de solicitud para archivos CSS y SVG
   if (request.url === "/styles.css") {
     filePath = path.join(__dirname, "styles.css");
   } else if (request.url === "/food-dish-svgrepo-com.svg") {
     filePath = path.join(__dirname, "food-dish-svgrepo-com.svg");
   } else {
-    filePath = path.join(__dirname, "index.html");
+    filePath = path.join(__dirname, htmlFileName);
   }
 
   fs.readFile(filePath, "utf8", (err, data) => {
     if (err) {
-      // Manejo de errores
       console.error(err);
       response.writeHead(500, { "Content-Type": "text/plain" });
-      response.end("Error interno del servidor");
+      response.end("Internal Server Error");
       return;
     }
 
-    // Determinar el tipo de contenido en función de la extensión del archivo
     const extname = path.extname(filePath);
     let contentType = "text/html";
 
@@ -36,7 +33,9 @@ const server = http.createServer((request, response) => {
     response.writeHead(200, { "Content-type": contentType });
     response.end(data);
   });
-});
+}
+
+const server = http.createServer((request, response) => {});
 
 server.listen(3000, () => {
   console.log("Servidor escuchando en http://localhost:3000");
