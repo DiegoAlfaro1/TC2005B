aa4b0012db631e3397a5cabdb1807b669d96a9fd
DiegoRAP2003 <diegoalfaropinto12@gmail.com>
2024-03-30 10:10:01 -0600

    problema con funcion .compare

--- MagnusStella-develop/controllers/usuarios.controller.js
+++ MagnusStella-develop/controllers/usuarios.controller.js
@@ -1,5 +1,5 @@
 const Usuarios = require("../models/usuarios.model");
-const bycrypt = require('bcryptjs');
+const bcrypt = require('bcryptjs');
 
 exports.get_login = (request, response, next) => {
     const error = request.session.error || '';
@@ -13,9 +13,57 @@ exports.post_login = (request, response, next) => {
     if(!email || !password){
         return response.render("login", {error: "Llena todos los campos"})
     }
-    request.session.username = request.body.name;
-    response.redirect('/');
+
+    // Usuarios.findByEmail(email,password)
+    //     .then(({user,passwordMatch}) => {
+    //         if(user && passwordMatch){
+    //             request.session.isLoggedIn = true;
+    //             request.session.user = user;
+    //             return request.session.save(err => {
+    //                 response.redirect('/');
+    //             });
+    //         }else{
+    //             response.redirect('/login');
+    //         }
+    //     })
+    //     .catch(err => {
+    //         console.error('Error durante el login',err);
+    //         response.redirect('/users/login')
+    //     })
+
+    Usuarios.findByEmail(email)
+        .then(({ user, passwordMatch }) => {
+            if (user && passwordMatch) {
+                // Use bcrypt.compare to check if passwords match
+                bcrypt.compare(password, user.Contrasena)
+                    .then(doMatch => {
+                        if (doMatch) {
+                            request.session.isLoggedIn = true;
+                            request.session.user = user;
+                            return request.session.save(err => {
+                                response.redirect('/');
+                            });
+                        } else {
+                            response.redirect('/login');
+                        }
+                    })
+                    .catch(err => {
+                        console.error('Error during login', err);
+                        response.redirect('/login');
+                    });
+            } else {
+                response.redirect('/login');
+            }
+        })
+        .catch(err => {
+            console.error('Error during login', err);
+            response.redirect('/login');
+        });
+
 };
+
+//contra Hola123
+//pruebaauth02@gmail.com
 
 exports.get_logout = (request, response, next) => {
     request.session.destroy(() => {
@@ -29,7 +77,7 @@ exports.get_signup =(request, response, next) => {
     response.render("signup")
 };
 
-let latestID = 4; //en este caso empiezo en 1 ya que ya se creo el primer usuario
+let latestID = 6; //en este caso empiezo en 1 ya que ya se creo el primer usuario
 
 exports.post_signup = (request,response,next) => {
     const {name, email, password} = request.body;
@@ -44,8 +92,14 @@ exports.post_signup = (request,response,next) => {
     const IdRol = Math.floor(Math.random() * 11);
 
     const usuarios = new Usuarios(name,email,password,idUser,IdRol);
-    usuarios.save();
-    response.redirect('/users/login');
+    usuarios.save()
+        .then(() =>{
+            response.redirect('/users/login')
+        })
+        .catch(err => {
+            console.log("Error al hacer el signup:",err);
+            response.redirect('/signup')
+        });
 }
 
 const generateUserID = () => {
